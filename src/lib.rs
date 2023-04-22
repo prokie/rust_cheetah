8a30e5587a30af9596ee7b0d84c2c6602400f052
Pontus <36114799+prokie@users.noreply.github.com>
2023-04-22 20:09:23 +0200

    Added mutex to solve issue with races

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+use std::sync::Mutex;
+
+lazy_static::lazy_static! {
+    static ref CH_FIND_DEVICES_MUTEX: Mutex<()> = Mutex::new(());
+}
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub fn find_devices() -> i32 {
-    let my_speed_ptr: *mut u16 = &mut 0;
-    unsafe { ch_find_devices(0, my_speed_ptr) }
+    let _lock = CH_FIND_DEVICES_MUTEX.lock().unwrap();
+    unsafe { ch_find_devices(0, &mut 0) }
 }
 
 #[cfg(test)]
@@ -15,6 +20,11 @@ mod tests {
 
     #[test]
     fn test_find_devices() {
+        assert!(find_devices() == 0);
+    }
+
+    #[test]
+    fn test_find_devices_igen() {
         assert!(find_devices() == 0);
     }
 }
