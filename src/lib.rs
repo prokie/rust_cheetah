7ef2f3ced6dae79629ecb45dc22cf36767218e1c
Pontus <36114799+prokie@users.noreply.github.com>
2023-04-22 22:38:07 +0200

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,16 @@ lazy_static::lazy_static! {
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-pub fn find_devices() -> i32 {
+pub fn find_devices() -> (i32, i32) {
     let _lock = CH_FIND_DEVICES_MUTEX.lock().unwrap();
-    unsafe { ch_find_devices(0, &mut 0) }
+    let devices = &mut 0;
+    let _ret = unsafe { ch_find_devices(0, devices) };
+    (_ret, *devices as i32)
+}
+
+pub fn find_devices_ext() -> i32 {
+    let _lock = CH_FIND_DEVICES_MUTEX.lock().unwrap();
+    unsafe { ch_find_devices_ext(0, &mut 0, 0, &mut 0) }
 }
 
 #[cfg(test)]
@@ -20,11 +27,8 @@ mod tests {
 
     #[test]
     fn test_find_devices() {
-        assert!(find_devices() == 0);
-    }
-
-    #[test]
-    fn test_find_devices_igen() {
-        assert!(find_devices() == 0);
+        let (ret, devices) = find_devices();
+        assert_eq!(ret, 0);
+        assert_eq!(devices, 0);
     }
 }
